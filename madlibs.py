70bc2a26526aaeb24cb8cb29d96405aacad587a4
sskarkigit <77776636+sskarkigit@users.noreply.github.com>
2021-02-04 12:31:08 +0545

    Delete madlibs.py

--- madlibs.py
+++
@@ -1,9 +0,0 @@
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous Person: ")
-
-madlib = f"Python programming is so {adj}! It makes me so excited all the time because \
-    I love to {verb1}. Stay hdrated and {verb2} like you are {famous_person}"
-
-print(madlib)
